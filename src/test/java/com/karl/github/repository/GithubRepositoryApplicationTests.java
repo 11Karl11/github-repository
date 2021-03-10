5bbeaac4944c381d8da74e354072470b503fa8e9
karl <xiezw@ruhrtec.cn>
2021-03-10 13:38:30 +0800

    first

---
+++ src/test/java/com/karl/github/repository/GithubRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karl.github.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
